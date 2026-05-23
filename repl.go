2c112f88eb45bdfb267f33fce3b2293c6892e52e
dlee9240 <dlee9240@gmail.com>
2026-05-22 17:03:35 -0700

    Finished Test Module

--- repl.go
+++ repl.go
@@ -1,7 +1,30 @@
 package main
 
+import (
+	"strings"
+)
+
 func cleanInput(text string) []string {
 	//return an empty string for now
-	return []string{}
-
+	//return []string{}
+	//takes a text string and splits it into a slice of strings.. Remove the whitespace etc
+	//var result []string
+	result := strings.Fields(strings.ToLower(text))
+	return result
 }
+
+//this is boot.dev solution
+/*
+package main
+
+import (
+	"strings"
+)
+
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
+
+*/
--- repl_test.go
+++ repl_test.go
@@ -8,17 +8,94 @@ func TestCleanInput(t *testing.T) {
 		input    string
 		expected []string
 	}{
-		input:    " hello world ",
-		expected: []string{"hello", "world"},
+		{
+			input:    "hello world",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    " hello world ",
+			expected: []string{"hello", "world"},
+		},
+		{ //second case
+			input:    " Daniel Lee is the best  person",
+			expected: []string{"daniel", "lee", "is", "the", "best", "person"},
+		},
+		{ //third case
+			input:    " Test CASE 3",
+			expected: []string{"test", "case", "3"},
+		},
 	}
+
 	//add more cases here
 
 	for _, c := range cases {
 		actual := cleanInput(c.input)
+		//check the length of the actual slice against the expected slice:
+		//if they don't match, use t.Errorf to print an error message and fail the test
+		if len(actual) != len(c.expected) {
+			t.Errorf("cleanInput(%q) == %q, want %q", c.input, actual, c.expected)
+			continue
+		}
 
 		for i := range actual {
 			word := actual[i]
 			expectedWord := c.expected[i]
+			//need to finish the function
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+			if word != expectedWord {
+				t.Errorf("got %q, expected %q", word, expectedWord)
+			}
+
 		}
 	}
 }
+
+/*
+This is boot.dev solution...
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  ",
+			expected: []string{},
+		},
+		{
+			input:    "  hello  ",
+			expected: []string{"hello"},
+		},
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  HellO  World  ",
+			expected: []string{"hello", "world"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("lengths don't match: '%v' vs '%v'", actual, c.expected)
+			continue
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("cleanInput(%v) == %v, expected %v", c.input, actual, c.expected)
+			}
+		}
+	}
+}
+*/
