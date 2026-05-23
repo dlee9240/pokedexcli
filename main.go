362d2673ed487f3f4f5ac9fac7abcf7385091069
dlee9240 <dlee9240@gmail.com>
2026-05-22 17:29:53 -0700

    Had to add relp.log

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	//remove hello world logic
+	//fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		//strings.ToLower(text)
+		result := strings.SplitN(strings.ToLower(text), " ", 2)
+		fmt.Printf("Your command was: %s\n", result[0])
+	}
 }
