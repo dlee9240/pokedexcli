c0d284249d3ed230ee38be799a1ec93d0d172174
dlee9240 <dlee9240@gmail.com>
2026-05-29 16:24:14 -0700

    Finished the cache module. Merge to main

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,16 +3,21 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/dlee9240/pokedexcli/internal/pokecache"
 )
 
 type Client struct {
 	//create a struct member that is an http.Client
+	cache      pokecache.Cache
 	httpClient http.Client
 }
 
 // New Client
-func NewClient(timeout time.Duration) Client {
+// updated NewClient arguments
+func NewClient(timeout, cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -12,35 +12,69 @@ type cacheEntry struct {
 
 type Cache struct {
 	//use above struct for the cacheEntry variable
-	cacheEntry map[string]cacheEntry
+	cache map[string]cacheEntry
 	//unsure if this type is correct or not...
-	cacheMutex sync.Mutex
+	//made it a pointer???
+	cacheMutex *sync.Mutex
 }
 
 // **** function to create a new Cache ****
 func NewCache(interval time.Duration) Cache {
 	//call readLoop() function!!!
 	//create and return the new cache.
+	c := Cache{
+		//creates a new cache map... set to empty values
+		cache: make(map[string]cacheEntry),
+		//need to understand this better
+		//seems to create a new mutex and stores the address of the mutex.
+		cacheMutex: &sync.Mutex{},
+	}
+
+	//use readloop function in a routine "go"
+	go c.readLoop(interval)
+	//return the cache after readall executes
+	return c
 }
 
 // might need to be a pointer?
-func (c *Cache) Add(key string, val []byte) {
+func (c *Cache) Add(key string, value []byte) {
 	//storing the passed in information into the map for the cache it points to...
 	//adding by time.Now() at the time it executes
-	c.cacheEntry[key] = cacheEntry{time.Now(), val}
+	//lock the mutex when Add function is called...
+	c.cacheMutex.Lock()
+	//defer to unlock the mutex in the struct when done
+	defer c.cacheMutex.Unlock()
+
+	//update the cache via the struct format...
+	c.cache[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       value,
+	}
 }
 
 // I think I got this one!!!
 func (c *Cache) Get(key string) ([]byte, bool) {
 	//bool should be true if found and false if not found
 	//return the cache record if found and the value if found or not.
-	result, exists := c.cacheEntry[key]
-	if exists {
-		//if the key exists then return the cache val ([]bytes) and return true.
-		return result.val, true
-	}
+
+	//forgot the c.mux.Lock
+	c.cacheMutex.Lock()
+	defer c.cacheMutex.Unlock()
+
+	//I think this part was wrong.. Going to comment it out and use the one in the solution
+	//result, exists := c.cache[key]
+	//if exists {
+	//if the key exists then return the cache val ([]bytes) and return true.
+	//	return result.val, true
+	//}
 	//otherwise return an empty result and false not found
-	return nil, false
+	//return nil, false
+
+	//from solution at boot.dev
+	val, ok := c.cache[key]
+
+	//return the value of whatever is in cache[key] and the error...
+	return val.val, ok
 
 }
 
@@ -51,5 +85,28 @@ func (c *Cache) readLoop(interval time.Duration) {
 	//creating a new ticker with the interval
 	ticker := time.NewTicker(interval)
 	//stopping at ticker implementation... step 4. Need to review when picking this shit up again....
+	//Reading from ticker.C blocks the goroutine until the specified duration has passed.
+	for range ticker.C {
+		//passes in the information in ticker.C (channel) and reaps...
+		c.reap(time.Now().UTC(), interval)
+	}
+}
 
+func (c *Cache) reap(now time.Time, last time.Duration) {
+	c.cacheMutex.Lock()
+	defer c.cacheMutex.Unlock()
+
+	// now = the current time Now().UTC() when the function was called
+	// last = interval (5 minutes)
+
+	//k = key
+	//v = value
+	//loop through the key value pairs in the cache map
+	for k, v := range c.cache {
+		//checks to see if the value in the cache map createdAt is before the current time -last //interval
+		if v.createdAt.Before(now.Add(-last)) {
+			//if so, delete the record from the cache with the key k variable from the for loop
+			delete(c.cache, k)
+		}
+	}
 }
